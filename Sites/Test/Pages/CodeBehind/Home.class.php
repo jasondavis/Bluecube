5b2d285800d9dd0e43ce7d25210bb867eb8b9a12
Borys Forytarz <borys.forytarz@gmail.com>
2012-10-09 17:29:23 +0200

    added more advanced example site

--- Sites/Test/Pages/CodeBehind/Home.class.php
+++ Sites/Test/Pages/CodeBehind/Home.class.php
@@ -1,13 +1,25 @@
 <?php
-class Home extends TPage
+class Home extends Master
 {
-	protected function Page_Load()
-	{
+    protected function Page_Init() 
+    {
+        
+    }
+    
+    protected function Page_Load()
+    {
 
-	}
-
-	protected function Button_Click(TButton $sender)
-	{
-		$sender->Text = 'Hi There! Thanks for clicking!';
-	}
+    }
+	
+    protected function Button_Click($sender, $event)
+    {
+        $sender->Text = 'You clicked me!';
+        $this->TextBox->Text = 'You clicked the button!';
+        $this->ActionLink->Show();
+    }
+	
+    public function action1()
+    {
+        $this->SomeText->Text = 'Hello from action1!';
+    }
 }
--- Sites/Test/Pages/CodeBehind/Master.class.php
+++ Sites/Test/Pages/CodeBehind/Master.class.php
@@ -1,8 +1,13 @@
 <?php
 class Master extends TPage
 {
-	protected function Page_Load()
+	protected function Page_Init() 
 	{
-
+		
 	}
+	
+    protected function Page_Load()
+    {
+        
+    }
 }
